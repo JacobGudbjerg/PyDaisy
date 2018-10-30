8cd9f3207ef5755a7ed409d1305fa250bd0f307b
Signekyndingborgen <43959840+Signekyndingborgen@users.noreply.github.com>
2018-10-30 11:53:56 +0100

    div

--- Projects/STYR-N/BuildAndRun.py
+++ Projects/STYR-N/BuildAndRun.py
@@ -7,16 +7,19 @@ Created on Tue Oct 30 09:52:27 2018
 
 import sys
 import pandas as pd
+import os
 
 sys.path.append(r'../../pydaisy')
 from Daisy import *
 
-df = pd.read_excel(r'..\Soil\jord fordelt på kommuner.xlsx', sheet_name='Ark1')
+df = pd.read_excel(r'Test_S1-S3_4.xlsx', sheet_name='Sheet1')
 
-template = DaisyModel(r'setup_template.dai')
+template = DaisyModel(r'S1_test1.dai')
 
 for i in range(0,len(df)):
+    unique_name = df['Block'][i] +'_' + df['Field'][i]+'_' + str(df['Treatment'][i])
+    
+    filename = os.path.join(unique_name, 'setup.dai')
+    template.save_as(filename)
 
-    template.save_as(r'./columns/setup.dai')
-
-run_sub_folders(r'./columns', 'setup.dai')
+# run_sub_folders(r'./columns', 'setup.dai')
