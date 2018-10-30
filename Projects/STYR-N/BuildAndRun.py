161e856bf065229245a32ad4a303961a61628043
Signekyndingborgen <43959840+Signekyndingborgen@users.noreply.github.com>
2018-10-30 14:01:02 +0100

    div

--- Projects/STYR-N/BuildAndRun.py
+++ Projects/STYR-N/BuildAndRun.py
@@ -8,6 +8,7 @@ Created on Tue Oct 30 09:52:27 2018
 import sys
 import pandas as pd
 import os
+import copy
 
 sys.path.append(r'../../pydaisy')
 from Daisy import *
@@ -16,10 +17,28 @@ df = pd.read_excel(r'Test_S1-S3_4.xlsx', sheet_name='Sheet1')
 
 template = DaisyModel(r'S1_test1.dai')
 
+
 for i in range(0,len(df)):
-    unique_name = df['Block'][i] +'_' + df['Field'][i]+'_' + str(df['Treatment'][i])
+    if df['Year'][i]==1994:
+        unique_name = df['Block'][i] +'_' + df['Field'][i]+'_' + str(df['Treatment'][i])
+        newfile= copy.deepcopy(template)
+        
+    block = copy.deepcopy(template.Input['defaction'][1])
+    if str(df['Plowing'][i]) =='NaT':
+        block.Children.remove(block['plowing'][0])
+        block.Children.remove(block['seed_bed_preparation'][0])
+    else:
+        block['wait_mm_dd'][0].setvalue(df['Plowing'][i].month, 0)
+        block['wait_mm_dd'][0].setvalue(df['Plowing'][i].day, 1)
+        block['wait_mm_dd'][1].setvalue(df['Seeding'][i].month, 0)
+        block['wait_mm_dd'][1].setvalue(df['Seeding'][i].day, 1)
+
     
-    filename = os.path.join(unique_name, 'setup.dai')
-    template.save_as(filename)
+    
+    newfile.Input['defaction'][1].Children.extend(block.Children)
+
+    if df['Year'][i]==2010:    
+        filename = os.path.join(unique_name, 'setup.dai')
+        newfile.save_as(filename)
 
 # run_sub_folders(r'./columns', 'setup.dai')
