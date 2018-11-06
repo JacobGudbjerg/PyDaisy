f6de25fe270292f04bad03d7be2ad30cebd50c56
Signekyndingborgen <43959840+Signekyndingborgen@users.noreply.github.com>
2018-11-06 10:51:51 +0100

    dv

--- Projects/STYR-N/Build_run_ny.py
+++ Projects/STYR-N/Build_run_ny.py
@@ -17,14 +17,22 @@ from Daisy import *
 df = pd.read_excel(r'S1-3_1993_2010_UD4.xlsx', sheet_name='S1')
 
 template = DaisyModel(r'S1_test2.dai')
-
+i=0
+unique_name = str(df['block'][i]) +'_' + df['field'][i]+'_' + str(df['treatment'][i])
+newfile= copy.deepcopy(template)    
+block = newfile.Input['defaction'][1]
 
 for i in range(0,len(df)):
-    if df['Year'][i]==1994:
-        unique_name = df['block'][i] +'_' + df['field'][i]+'_' + str(df['treatment'][i])
-        newfile= copy.deepcopy(template)    
-        block = newfile.Input['defaction'][1]
-        
-    if str(df['Sow_crop1'][i]) !='nan':
-        block.Children.append(DaisyEntry('wait_mm_dd', [str(df['Plowing'][i].month), str(df['Plowing'][i].day)]))
-        block.Children.append(DaisyEntry('plowing', []))
+    block.Children.append(DaisyEntry('wait_mm_dd', [str(df['date'][i].month), str(df['date'][i].day)]))
+    action =DaisyEntry(df['action'][i], [])
+    
+    if df['action'][i]=='sow':
+        action.Children.extend(df['what'])
+    elif df['action'][i]=='harvest':
+        action.Children.extend(df['what'])
+    #elif df['action'][i]=='fertilize':
+     #   action.Children.append
+    
+    block.Children.append(action)
+filename = os.path.join(unique_name, 'setup.dai')
+newfile.save_as(filename)
