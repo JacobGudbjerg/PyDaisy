ba59a41f0c7116f14ea06aee591effccb9c4d082
Signekyndingborgen <43959840+Signekyndingborgen@users.noreply.github.com>
2018-11-08 15:47:52 +0100

    dv

--- Projects/STYR-N/Plots/test.py
+++ Projects/STYR-N/Plots/test.py
@@ -6,15 +6,35 @@ Created on Thu Nov  8 15:01:28 2018
 """
 
 
-sys.path.append(r'h:\Documents\PyDaisy')
+sys.path.append(r'h:\Documents\PyDaisy\Projects\STYR-N')
 
+sys.path.append(r'h:\Documents\PyDaisy')
 from pydaisy.Daisy import *
 dlf = DaisyDlf(r'h:\Documents\PyDaisy\Projects\STYR-N\IND_2_4\DailyP-harvest.dlf')
 
-df = dlf.Data  
+folder = r'..\IND_2_4'
+result_cropN_day = DaisyDlf(folder+'\DailyP-Ryegrass.dlf')
 
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+# sum daily values of plant N in leaf, stem and sorg
+N_rg = result_cropN_day.Data.values[:,10]+result_cropN_day.Data.values[:,12]+result_cropN_day.Data.values[:,13]
+
+result_cropN_day = DaisyDlf(folder+'\DailyP-Wclover.dlf')
+N_wc = result_cropN_day.Data.values[:,10]+result_cropN_day.Data.values[:,12]+result_cropN_day.Data.values[:,13]
+
+result_cropN_day = DaisyDlf(folder+'\DailyP-SB.dlf')
+N_sb = result_cropN_day.Data.values[:,10]+result_cropN_day.Data.values[:,12]+result_cropN_day.Data.values[:,13]
+
+
+
+#from os import listdir
+#from os.path import isfile, join
+#onlyfiles = [f for f in listdir(r'..\IND_1_4') if isfile(join('r..\IND_1_4', f))]
+
+
+
+#from pydaisy.Daisy import *
+#dlf = DaisyDlf(r'h:\Documents\PyDaisy\Projects\STYR-N\IND_2_4\DailyP-harvest.dlf')
+
+#df = dlf.Data  
 
 #
