8bf2e5fdb103744effcded8511bd80b7c377fe07
JacobGudbjerg <jacobgudbjerg@gmail.com>
2020-04-22 23:20:31 +0200

    restructured

--- setup.py
+++ setup.py
@@ -31,5 +31,5 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.6'],
+        'Programming Language :: Python :: 3.7'],
     )
